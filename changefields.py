acf2551e3aed85e95f54771e73f55bf0af816e61
zoveress <47656093+zoveress@users.noreply.github.com>
2021-03-12 12:33:39 +0400

    Update changefields.py

--- changefields.py
+++ changefields.py
@@ -7,7 +7,7 @@ print (indexdate)
 '''
 curl -vv -H 'Content-Type: application/json' -XPUT 'localhost:9200/sts_multi_metrics-2020.02.12/_settings' -d ' { "index.mapping.total_fields.limit": 5000 }' 
 '''
-command="curl -vv -H 'Content-Type: application/json' -XPUT '172.30.249.45:9200/sts_multi_metrics-"+indexdate+"/_settings' -d ' { "+'"'+"index.mapping.total_fields.limit"+'"'+": 5000 }'"
+command="curl -vv -H 'Content-Type: application/json' -XPUT '172.30.124.5:9200/sts_multi_metrics-"+indexdate+"/_settings' -d ' { "+'"'+"index.mapping.total_fields.limit"+'"'+": 5000 }'"
 print (command)
 #subprocess.check_output(command)
 myprocess=subprocess.run(command,shell=True,timeout=120)
